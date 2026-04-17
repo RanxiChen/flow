e65fd230798ca408418b064172abbbcdd8d1dfbb
chenxiuzhuang <11393958+chenxiuzhuang@user.noreply.gitee.com>
2026-04-17 14:23:16 +0800

    add standalone fetch buffer

---
+++ design/src/main/scala/buffer/fetchbuffer.scala
@@ -0,0 +1,35 @@
+package flow.buffer
+
+import chisel3._
+import chisel3.util._
+import flow.interface._
+
+class FetchBuffer(val VLEN: Int = 64, val entries: Int = 4) extends Module {
+    require(entries >= 3, "FetchBuffer entries must be at least 3 to support canAccept3")
+
+    val io = IO(new Bundle {
+        val in = Flipped(new FrontendFetchBufferIO(VLEN))
+        val out = Decoupled(new FrontendFetchBundle(VLEN))
+    })
+
+    val fifo = Module(
+        new Queue(
+            gen = new FrontendFetchBundle(VLEN),
+            entries = entries,
+            pipe = false,
+            flow = false
+        )
+    )
+
+    fifo.io.enq.valid := io.in.valid
+    fifo.io.enq.bits := io.in.bits
+    io.out <> fifo.io.deq
+
+    val freeEntries = entries.U - fifo.io.count
+    io.in.canAccept3 := freeEntries >= 3.U
+
+    assert(
+        !io.in.valid || fifo.io.enq.ready,
+        "FetchBuffer overflow: frontend pushed data when FIFO could not accept it"
+    )
+}
